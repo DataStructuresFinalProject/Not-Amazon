5176e448ce920fa352d6d876e52853ec42292d9d
bryanmurphy <bryanmurphy@localhost>
2021-11-14 15:16:33 -0500

    I am pushing testingItems

---
+++ src/main/java/com/amazon/testingItems.java
@@ -0,0 +1,33 @@
+package com.amazon;
+
+public class testingItems {
+
+	public static void main(String[] args) {
+		
+		item item1 = new item("Toothbrush", 5.0, 10, 1);
+		item item2 = new item("Red Sox Hat", 30.0, 5, 2);
+		item item3 = new item("Lawn Chair", 25.0, 30, 3);
+		item item4 = new item("Eraser", 3.0, 1000, 4);
+		item item5 = new item("Whiteboard", 20.0, 555, 5);
+		item item6 = new item("MacBook", 1000.0, 0, 6);
+		item item7 = new item("Waterbottle", 15.0, 100, 7);
+		item item8 = new item("Pillow", 10.0, 900, 8);
+		item item9 = new item("Blanket", 45.0, 62, 9);
+		item item10 = new item("Keyboard", 150.0, 9, 10);
+		item item11= new item("Monitor", 200.0, 0, 11);
+		item item12 = new item("Mousepad", 50.0, 32, 12);
+		item item13 = new item("Hose", 25.0, 510, 13);
+		item item14 = new item("Bannana", 5.0, 59, 14);
+		item item15 = new item("Case of water", 15.0, 322, 15);
+		item item16 = new item("Hdmi cable", 10.0, 0, 16);
+		item item17 = new item("Binder", 7.50, 14, 17);
+		item item18 = new item("Shoe", 99.99, 76, 18);
+		item item19 = new item("Shoe laces", 4.99, 76, 19);
+		item item20 = new item("Socks", 9.99, 42, 20);
+		
+		
+		
+
+	}
+
+}
