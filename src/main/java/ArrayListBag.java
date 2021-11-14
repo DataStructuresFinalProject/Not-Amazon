bfeadcd8a932d3316efa7a15665f8c2fbf0bc293
Cole Thomas <cole@okemorental.com>
2021-11-14 15:09:58 -0500

    Added bagInterface and ArrayListBag classes

---
+++ src/main/java/ArrayListBag.java
@@ -0,0 +1,53 @@
+import com.amazon.BagInterface;
+
+public class ArrayListBag<T> implements BagInterface<T> {
+
+    @Override
+    public int getCurrentSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean add(T newEntry) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean remove(T anEntry) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getFrequencyOf(T anEntry) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean contains(T anEntry) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
