9950fc3d664cdecb859942cafdaa16ab1c711a6b
Cole Thomas <cole@okemorental.com>
2021-12-01 20:25:48 -0500

    Added methods and javadoc comments to node class

--- src/main/java/application/Node.java
+++ src/main/java/application/Node.java
@@ -5,25 +5,80 @@ public class Node<T> {
     Node<T> NextNode;
     int Index;
     
+    /**
+     * Default constructor
+     * Accepts data for the current node.
+     * @param data Data of the current node.
+     */
     public Node(T data){
         Data = data;
         Index=1;
     }
 
-    public Node(T data, int index){
+    /**
+     * Alternate constructor
+     * Accepts data, neighboring node, and index of the current node.
+     * @param data Data of the current node.
+     * @param nextNode The next neighboring node in the chain.
+     * @param index The index of the current node in the chain.
+     */
+    public Node(T data, Node<T> nextNode, int index){
         Data = data;
         Index = index;
+        NextNode = nextNode;
     }
 
+    
+    /** 
+     * Sets the next node relative to the current node.
+     * @param nextNode The next neighboring node in the chain.
+     */
     public void setNextNode(Node<T> nextNode) {
         NextNode = nextNode;
     }
 
+    
+    /** 
+     * Retrieves the next node in the chain.
+     * @return Node<T> The next neighboring node in the chain.
+     */
     public Node<T> getNextNode() {
         return NextNode;
     }
 
+    
+    /** 
+     * Sets the index of the current node in the list as a whole.
+     * @param index The new index to be assigned to the node.
+     */
     public void setIndex(int index) {
         Index = index;
+    }
+
+    
+    /** 
+     * Retrieves the index of the current node.
+     * @return int Current index of the Node.
+     */
+    public int getIndex() {
+        return Index;
+    }
+
+    
+    /** 
+     * Sets the data of the current node.
+     * @param data The data to be assigned to the node.
+     */
+    public void setData(T data) {
+        Data = data;
+    }
+
+    
+    /** 
+     * Retrieves the data of the current node.
+     * @return T The data held by the current node.
+     */
+    public T getData() {
+        return Data;
     }
 }
